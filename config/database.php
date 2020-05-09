cf5ce32c7f86e5c6cd6f361814e0c345db17f7c8
M. Jameel Baig <mjameelbaig@hotmail.com>
2020-05-10 06:11:41 +1000

    added db config

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'default' => 'mysql',
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => 'us-cdbr-east-06.cleardb.net',
+            'database' => 'heroku_dbcb05f48e6226d',
+            'username' => 'bb49b596994777',
+            'password' => 'c8ab0123',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+    ]
+];
